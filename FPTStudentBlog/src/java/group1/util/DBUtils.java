80ff47b2b6b977f4f7831c7326612d5d28d0efbf
kh0ala <khoalase150498@fpt.edu.vn>
2021-09-23 11:16:49 +0700

    Update

---
+++ FPTStudentBlog/src/java/group1/util/DBUtils.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group1.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Admin
+ */
+public class DBUtils {
+    public static Connection getConnection() throws ClassNotFoundException, SQLException{
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String url="jdbc:sqlserver://localhost:1433;databaseName=FPTStudentBlog";
+        Connection conn= DriverManager.getConnection(url, "sa", "123456789");
+        return conn;
+    }
+}
