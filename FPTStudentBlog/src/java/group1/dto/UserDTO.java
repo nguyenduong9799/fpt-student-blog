3c2eca4cf8e40571cce5bcbb491e0fcadddaefef
endynguyen <duongnse150483@fpt.edu.vn>
2021-09-23 10:47:06 +0700

    update

---
+++ FPTStudentBlog/src/java/group1/dto/UserDTO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group1.dto;
+
+
+public class UserDTO {
+     private String UserID;
+    private String name;
+    private String password;
+    private String phone;
+    private String email;
+    private String roleID;
+    private String rankID;
+    private int totalVote;
+}
