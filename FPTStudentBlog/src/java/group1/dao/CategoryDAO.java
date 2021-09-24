021dcd9065b697f9d08fa48db036b0b2e5ce84ef
endynguyen <duongnse150483@fpt.edu.vn>
2021-09-24 16:35:32 +0700

    Update CategoryDAO.java

--- FPTStudentBlog/src/java/group1/dao/CategoryDAO.java
+++ FPTStudentBlog/src/java/group1/dao/CategoryDAO.java
@@ -5,10 +5,44 @@
  */
 package group1.dao;
 
+import group1.dto.CategoryDTO;
+import group1.util.DBUtils;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  *
  * @author Admin
  */
 public class CategoryDAO {
-    
+    public static ArrayList<CategoryDTO> getAllCategory() throws SQLException{
+        ArrayList<CategoryDTO> list = null;
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.getConnection();
+            if(conn != null){
+                String sql = "Select categoryID, categoryName\n" +
+                             "From tblCategories \n";
+                stm = conn.prepareStatement(sql);
+                rs = stm.executeQuery();
+                while(rs.next()){
+                    if(list == null) list = new ArrayList<>();
+                    list.add(new CategoryDTO(rs.getInt("categoryID"),
+                            rs.getString("categoryName")));
+                }
+            }
+        } catch (Exception e) {
+            
+        } finally {
+            if(rs != null) rs.close();
+            if(stm != null) stm.close();
+            if(conn != null) conn.close();
+        }
+        return list;
+    }
 }
