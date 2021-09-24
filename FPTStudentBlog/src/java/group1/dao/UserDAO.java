8840ac465c87d8ab2e28f181fd9a3a98e25c9181
quangnt1702 <quangnt1702@gmail.com>
2021-09-24 20:39:27 +0700

    add

--- FPTStudentBlog/src/java/group1/dao/UserDAO.java
+++ FPTStudentBlog/src/java/group1/dao/UserDAO.java
@@ -59,6 +59,32 @@ public class UserDAO {
         }
         return user;
     }
+         public static String getUserNameByID(String userID) throws SQLException{
+       String userName = null;
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.getConnection();
+            if(conn != null){
+                String sql = "Select userName " +
+                             "From tblUsers where userID=?";
+                stm = conn.prepareStatement(sql);
+                stm.setString(1, userID);
+                rs = stm.executeQuery();
+                while(rs.next()){
+                   userName=rs.getString("userName");
+                }
+            }
+        } catch (Exception e) {
+            
+        } finally {
+            if(rs != null) rs.close();
+            if(stm != null) stm.close();
+            if(conn != null) conn.close();
+        }
+        return userName;
+    }
 //    public static void main(String[] args) throws SQLException {
 //        UserDAO dao = new UserDAO();
 //
