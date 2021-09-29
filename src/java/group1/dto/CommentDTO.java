2928f9b8d7bdb4051e0fc2b5f464d9df7ad7a830
khoa-la <khoalase150498@fpt.edu.vn>
2021-09-29 21:00:02 +0700

    Upload Comment

--- src/java/group1/dto/CommentDTO.java
+++ src/java/group1/dto/CommentDTO.java
@@ -10,5 +10,70 @@ package group1.dto;
  * @author Admin
  */
 public class CommentDTO {
+    private int commentID;
+    private int postID;
+    private String userID;
+    private String date;
+    private String commentContent;
+
+    public CommentDTO() {
+    }
+
+    public CommentDTO(int postID, String userID, String date, String commentContent) {
+        this.postID = postID;
+        this.userID = userID;
+        this.date = date;
+        this.commentContent = commentContent;
+    }
+
+    public CommentDTO(int commentID, int postID, String userID, String date, String commentContent) {
+        this.commentID = commentID;
+        this.postID = postID;
+        this.userID = userID;
+        this.date = date;
+        this.commentContent = commentContent;
+    }
+    
+    
+
+    public int getCommentID() {
+        return commentID;
+    }
+
+    public void setCommentID(int commentID) {
+        this.commentID = commentID;
+    }
+
+    public int getPostID() {
+        return postID;
+    }
+
+    public void setPostID(int postID) {
+        this.postID = postID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCommentContent() {
+        return commentContent;
+    }
+
+    public void setCommentContent(String commentContent) {
+        this.commentContent = commentContent;
+    }
     
 }
