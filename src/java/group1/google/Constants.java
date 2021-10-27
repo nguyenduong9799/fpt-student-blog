167acddbf0e895c3094c91a65de717c3eeb50cc5
endynguyen <duongnse150483@fpt.edu.vn>
2021-10-27 17:16:34 +0700

    update login google

---
+++ src/java/group1/google/Constants.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package group1.google;
+
+public class Constants {
+    public static String GOOGLE_CLIENT_ID = "695662211660-amed2ubedam4ppvavi64t0ghhb033crb.apps.googleusercontent.com";
+	public static String GOOGLE_CLIENT_SECRET = "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";
+	public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/fpt-student-blog/login-google";
+	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+	public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+	public static String GOOGLE_GRANT_TYPE = "authorization_code";
+}
